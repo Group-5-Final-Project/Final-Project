ea7de092fca0f396c0c2faa7b355fc0b53225023
farwaali08 <89050277+farwaali08@users.noreply.github.com>
2022-01-24 09:13:58 -0500

    database schema

---
+++ schema.sql
@@ -0,0 +1,33 @@
+CREATE TABLE tweets (
+    user_name varchar,
+    date dt,
+    text varchar,
+    hashtags varchar,
+    is_retweet boolean
+);
+
+CREATE TABLE Tweets_Chart (
+    date dt,
+    label varchar,
+    counts int
+);
+
+CREATE TABLE NLP_tweets (
+    user_name varchar,
+    date dt,
+    text varchar,
+    label varchar
+);
+
+********
+OPTIONAL
+
+ALTER TABLE tweets
+DROP COLUMN IF EXISTS index;
+
+ALTER TABLE tweets_chart 
+DROP COLUMN IF EXISTS index;
+
+ALTER TABLE NLP_tweets 
+DROP COLUMN IF EXISTS index;
+
