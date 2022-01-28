1b76d4469ecf6ec02e388e395af1c9dbdae37127
farwaali08 <89050277+farwaali08@users.noreply.github.com>
2022-01-28 18:48:18 -0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -21,8 +21,13 @@ CREATE TABLE NLP_tweets (
     label varchar
 );
 
-********
-OPTIONAL
+CREATE TABLE encoded_data (
+    text varchar,
+    label int, 
+);
+
+-----------------
+-- OTHER OPERATIONS THAT WERE PERFORMED
 
 ALTER TABLE tweets
 DROP COLUMN IF EXISTS index;
@@ -32,4 +37,13 @@ DROP COLUMN IF EXISTS index;
 
 ALTER TABLE NLP_tweets 
 DROP COLUMN IF EXISTS index;
+
+ALTER TABLE "encoded_data"
+DROP COLUMN IF EXISTS index;
+
+ALTER TABLE "NLP_tweets"
+RENAME COLUMN label to sentiment;
+
+-----------------
+-- JOINING "NLP_tweets" & "encoded_data" 
 
